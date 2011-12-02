11b0a12d34f3de3787913ba693207ffbc2da1099
Marcin Bunsch <marcin@futuresimple.com>
2011-12-02 21:19:44 +0100

    Cleaning up the hooks - adding after_second_pass, which is the final hook before the result is returned
    Also, making after_first_pass an alias to after_prepare for legacy purposes. Running after_prepare in the middle of the prepare method hurts me deeply.

--- lib/representer/modules/preparation.rb
+++ lib/representer/modules/preparation.rb
@@ -18,22 +18,17 @@ module Representer
         before_prepare
         prepared = @representable.collect { |item| first_pass(item) }
         prepared = after_first_pass(prepared)
-        prepared = after_prepare(prepared)
+        prepared.each { |item| second_pass(item) } unless skip_second_pass?
+        prepared = after_second_pass(prepared)
         @returns_many ? prepared : prepared[0]
       end
 
       def after_first_pass(prepared)
         prepared
       end
+      alias :after_prepare :after_first_pass
 
-      def after_prepare(prepared)
-        # Do not perform a second pass when there is no need
-        return prepared if skip_second_pass?
-
-        # Run second_pass on each prepared item
-        # Second pass will modify the item, so we don't need to capture the output
-        prepared.each { |item| second_pass(item) }
-
+      def after_second_pass(prepared)
         prepared
       end
 
